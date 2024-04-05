ea3736da31b633e0ef004ca5bdb52f575c2b514f
Sarthak Teotia <sarthak.clevdoc@gmail.com>
2024-04-05 21:45:58 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -7,6 +7,14 @@ function App() {
   const [key, setKey] = useState('');
   const [expiryTimeFrame, setExpiryTimeFrame] = useState('1');
   const [userData, setUserData] = useState([]);
+  const [copiedData, setCopiedData] = useState(null);
+
+  const copyUserData = (username, key) => {
+    const dataToCopy = `${username}: ${key}`;
+    navigator.clipboard.writeText(dataToCopy)
+      .then(() => setCopiedData(dataToCopy))
+      .catch((error) => console.error('Failed to copy:', error));
+  };
 
   const handleExpiryTimeFrameChange = (e) => {
     setExpiryTimeFrame(e.target.value);
@@ -15,7 +23,12 @@ function App() {
   const fetchAllUserData = async () => {
     try {
       const response = await axios.get('https://server-j98j.onrender.com/api/user');
-      setUserData(response.data);
+      const modifiedUserData = response.data.map(user => {
+        const expiresInMs = new Date(user.expires_in) - new Date();
+        const expiresInDays = Math.ceil(expiresInMs / (1000 * 60 * 60 * 24));
+        return { ...user, expiresInDays };
+      });
+      setUserData(modifiedUserData);
     } catch (error) {
       console.error('Error fetching user data:', error);
     }
@@ -25,12 +38,19 @@ function App() {
     fetchAllUserData();
   }, []);
 
+
+  useEffect(() => {
+    fetchAllUserData();
+  }, []);
+
   const postUserData = async (username, key) => {
     try {
+      const currentDate = new Date(); // Get current date and time
       const userDataResponse = await axios.post('https://server-j98j.onrender.com/api/generate', {
         username: username,
         key: key,
-        expiry: expiryTimeFrame
+        expiry: expiryTimeFrame,
+        generated_at: currentDate, // Convert date to string
       });
       
       console.log('User data POST response:', userDataResponse.data);
@@ -39,7 +59,7 @@ function App() {
       console.error('Error posting user data:', error);
     }
   };
-
+  
   const deleteUser = async (username) => {
     try {
       const response = await axios.delete(`https://server-j98j.onrender.com/api/user/${username}`);
@@ -51,7 +71,7 @@ function App() {
   };
 
   const generateUserData = () => {
-    const randomUsername = 'User_' + Math.floor(Math.random() * 1000);
+    const randomUsername = 'user_' + Math.floor(Math.random() * 10000);
     setUsername(randomUsername);
     const randomKey = Math.random().toString(36).substring(7);
     setKey(randomKey + ' (Expires in ' + expiryTimeFrame + ')');
@@ -61,12 +81,12 @@ function App() {
   return (
     <div className="App">
       <div className="input-container1">
-      <h1>User Creation</h1>
+        <h1>User Creation</h1>
         <label className="label">Expiry Time Frame:</label>
-        <select value={expiryTimeFrame} onChange={handleExpiryTimeFrameChange}>
-          <option value="1">1</option>
-          <option value="3">3</option>
-          <option value="7">7</option>
+        <select className="dropdown" value={expiryTimeFrame} onChange={handleExpiryTimeFrameChange}>
+          <option className="dropdown-option" value="1">1</option>
+          <option className="dropdown-option" value="3">3</option>
+          <option className="dropdown-option" value="7">7</option>
         </select>
       </div>
       <div className="input-container">
@@ -84,18 +104,23 @@ function App() {
           <tr>
             <th>Username</th>
             <th>Key</th>
-            <th>Action</th> {/* Add new header for action */}
+            <th>Generated At</th>
+            <th>Action</th>
           </tr>
         </thead>
         <tbody>
-          {userData.map((user, index) => (
-            <tr key={index}>
-              <td>{user.username}</td>
-              <td>{user.key}</td>
-              <td><button onClick={() => deleteUser(user.username)}>Delete</button></td> {/* Delete button */}
-            </tr>
-          ))}
-        </tbody>
+      {userData.map((user, index) => (
+        <tr key={index}>
+          <td>{user.username} ({user.expiresInDays})</td>
+          <td>{user.key}</td>
+          <td>{user.generated_at ? new Date(user.generated_at).toLocaleString() : ''}</td>
+          <td>
+            <button className='copy' onClick={() => copyUserData(user.username, user.key)}>Copy</button>
+            <button onClick={() => deleteUser(user.username)}>Delete</button>
+          </td>
+        </tr>
+      ))}
+    </tbody>
       </table>
     </div>
   );
